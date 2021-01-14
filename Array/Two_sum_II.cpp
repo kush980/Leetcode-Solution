43c58fba2b16c719a5387cad07f0c9a0fa2a1530
kushagr <kushagrsinghdec1@gmail.com>
2021-01-14 17:13:05 +0530

    Two pointer approach

---
+++ Array/Two_sum_II.cpp
@@ -0,0 +1,20 @@
+//Question link
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int l = 0,r=nums.size()-1;
+        
+        while(l<r)
+        {
+            if(nums[l]+nums[r]==target)
+                return {l+1,r+1};
+            else if(nums[l]+nums[r]>target)
+                r--;
+            else
+                l++;
+        }
+        return {-1};
+    }
+};
